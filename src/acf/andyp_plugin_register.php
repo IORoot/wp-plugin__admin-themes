44f15688881a5091c2d105002215b68d3312903c
Andy Pearson <andy@londonparkour.com>
2021-01-02 15:43:18 +0000

    Change title

--- src/acf/andyp_plugin_register.php
+++ src/acf/andyp_plugin_register.php
@@ -2,7 +2,7 @@
 
 add_action( 'plugins_loaded', function() {
     do_action('register_andyp_plugin', [
-        'title'     => 'Admin - Remove Comments',
+        'title'     => 'Admin - CSS Themes',
         'icon'      => 'monitor-dashboard',
         'color'     => '#b71c1c',
         'path'      => __FILE__,
